f38e7be4c1fc725194ec19e245530c23fb70abf0
luisana muñoz <luisanamunoz879@gmail.com>
2023-02-15 16:17:25 -0300

    btn-home y btn-cancelar(función agregada)

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ const isHiddenForm = (e) =>{
 }
 document.getElementById("btn-create").addEventListener("click", isHiddenForm)
 document.getElementById("btn-create2").addEventListener("click", isHiddenForm)
+
+
+
+// función de btn home y btn de cancelar operación
+const isHiddenHome = ("click", (e) =>{
+    window.location.reload()
+})
+document.getElementById("btn-home").addEventListener("click", isHiddenHome)
+document.getElementById("btn-cancelar").addEventListener("click", isHiddenHome)
