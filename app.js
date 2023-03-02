70d964be7eb0fe964039ef1ae8609a7771d5fcf8
luisana muñoz <luisanamunoz879@gmail.com>
2023-03-02 12:17:43 -0300

    funcionalidad de subir obj

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // función de btn burge
 const menuBurgerBtn = (e) => document.getElementById(`div-burger`).classList.toggle(`oculto`)
+
 document.getElementById("btnBurge").addEventListener(`click`, menuBurgerBtn)
 
 
@@ -18,4 +19,52 @@ const isHiddenHome = ("click", (e) =>{
     window.location.reload()
 })
 document.getElementById("btn-home").addEventListener("click", isHiddenHome)
+document.getElementById("btn-home2").addEventListener("click", isHiddenHome)
 document.getElementById("btn-cancelar").addEventListener("click", isHiddenHome)
+
+
+// BASE URL 
+const BASE_URL = "https://63eb9028bfdd4299674a8f24.mockapi.io/api/";
+
+const $ = (selector) => document.querySelector(selector);
+
+// CREAR ARTICULO (METODO POST)
+const registerJob = async () => {
+  try {
+    const article = getJobForm();
+
+    const response = await fetch(`${BASE_URL}/articles`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json'
+      },
+      body: JSON.stringify(article),
+    });
+
+    const jobs = await response.json();
+  
+  } catch (error) {
+  alert("Error creating job");
+
+  } finally {
+  window.location.href = "index.html";
+  }
+};
+
+const getJobForm = () => {
+  return {
+      title: $("#title-input").value,
+      description: $("#description-input").value,
+      location: $("#location-select").value,
+      category: $("#category-select").value,
+      seniority: $("#seniority-select").value,
+    };
+};
+
+$("#form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  registerJob();
+});
+
+
+
